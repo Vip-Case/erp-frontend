146752105ac480e34b9e924fa52b07992274a1c3
ersaayan <ersaayan@gmail.com>
2024-10-25 11:03:10 +0300

    Refactor Categories component structure and add toolbar, tree list, and dialog components

--- components/Categories/index.tsx
+++ components/Categories/index.tsx
@@ -1,31 +1,21 @@
 'use client';
 
 import React from 'react';
-import { Button } from "@/components/ui/button";
-import { Plus, RefreshCw } from 'lucide-react';
-import { useCategoryDialog } from './useCategoryDialog';
+import { Card } from "@/components/ui/card";
+import CategoriesToolbar from './CategoriesToolbar';
+import CategoriesTreeList from './CategoriesTreeList';
+import CategoryDialog from './CategoryDialog';
 
-const CategoriesToolbar: React.FC = () => {
-    const { openDialog } = useCategoryDialog();
-
+const Categories: React.FC = () => {
     return (
-        <div className="flex justify-end items-center gap-2">
-            <Button variant="outline" size="sm">
-                <RefreshCw className="h-4 w-4 mr-2" />
-                Yenile
-            </Button>
-
-            <Button 
-                variant="default" 
-                size="sm" 
-                className="bg-[#84CC16] hover:bg-[#65A30D]"
-                onClick={openDialog}
-            >
-                <Plus className="h-4 w-4 mr-2" />
-                Kategori Ekle
-            </Button>
+        <div className="grid-container">
+            <CategoriesToolbar />
+            <Card className='mt-4'>
+                <CategoriesTreeList />
+            </Card>
+            <CategoryDialog />
         </div>
     );
 };
 
-export default CategoriesToolbar;
+export default Categories;
